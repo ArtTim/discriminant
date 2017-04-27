222a2ab53191f7887c2440d7faa9dc00b2d80b79
Стажирова <911>
2017-04-27 14:39:57 +0300

    Добавлена форма ввода данных

--- index.php
+++ index.php
@@ -7,6 +7,13 @@
 <body>
 <?php
 require_once ('php/formula.php');
+require_once ('php/abc.php');
 ?>
+<form action="foo.php" method="post">
+    Имя:  <input type="text" name="username" /><br />
+    Email: <input type="text" name="email" /><br />
+    <input type="submit" name="submit" value="Отправь меня!" />
+</form>
+
 </body>
 </html>
