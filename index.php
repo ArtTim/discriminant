0221b7f4d48cea91674dbd8cfdc34bccd2e78c30
Стажирова <911>
2017-04-27 11:37:27 +0300

    First upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Вычисление дискриминанта</title>
+</head>
+<body>
+<?php
+require_once ('php/formula.php');
+?>
+</body>
+</html>
