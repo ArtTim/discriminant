ba4a0f95318a9a6bc80b85c9c9bb805a844fd5bd
Стажирова <911>
2017-04-28 19:12:47 +0300

    Корректировка Васи

--- php/formula.php
+++ php/formula.php
@@ -15,23 +15,31 @@ echo $c . '<br>';
 */
 
 $D = $b**2-4*$a*$c;
-echo 'Дискриминант равен '. $D . '<br>';
+?>
 
-if ($D>0) {
+Дискриминант равен <?=$D;?><br>
+
+<?PHP if ($D>0) :?>
+    <?php
     $x1 = (-$b-bcsqrt($D))/(2*$a);
     $x2 = (-$b+bcsqrt($D))/(2*$a);
-    echo '<math>' . '<mfrac beveled="true">' . '<mi>' . -$b . '-' . '<span class="radical">' . '&radic;' . $D . '</span>' . '</mi>' . '<mi>' . '2' . '*' . $a . '=' . $x1 . '</mi>' . '</mfrac>' . '</math>';
-
-
-    echo 'x1 = ' . $x1 . '<br>';
-    echo 'x2 = ' . $x2 . '<br>';
-}
-    elseif ($D<0) {
-        echo 'Корней нет' . '<br>';
-    }
-        elseif ($D=0) {
-
-        }
-
-?>
+    ?>
+    <math> <mfrac beveled="true">
+            <mi>
+                <?=-$b;?> -
+                <span class="radical">&radic;<?=$D;?></span>
+            </mi>
+            <mi>
+                2*<?=$a;?>=<?=$x1;?>
+            </mi>
+        </mfrac>
+    </math>
+    <br>
+    x1 = <?=$x1;?><br>
+    x2 = <?=$x2;?><br>
+    <? elseif($D<0):?>
+        Корней нет<br>
+    <?php else : ?>
+        d=0
+<?php endif;?>
 <a href="../index.php">Вернуться</a>
